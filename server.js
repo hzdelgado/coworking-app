d0e2b4d0722a95de91d3b2b511cf5cd2679e79ab
hzorrilla <152939685+hzorrilla@users.noreply.github.com>
2024-11-29 06:47:59 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Servir los archivos estáticos desde la carpeta 'dist'
+app.use(express.static(path.join(__dirname, 'dist/your-app-name')));
+
+// Redirigir todas las rutas no definidas a index.html
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/your-app-name', 'index.html'));
+});
+
+// Configurar el puerto
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
