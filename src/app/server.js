dabc2b9c3ef0a60136dfb0aaf7498fbd6ffecac2
hzorrilla <152939685+hzorrilla@users.noreply.github.com>
2024-11-29 07:01:34 -0500

    path to index.html updated

---
+++ src/app/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+
+// Servir los archivos estáticos desde la carpeta 'dist'
+app.use(express.static(path.join(__dirname, 'dist/coworking-app/browser')));
+
+// Redirigir todas las rutas no definidas a index.html
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/coworking-app/browser', 'index.html'));
+});
+
+// Configurar el puerto
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
